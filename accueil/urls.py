d3f011e725fa9aec6abb9278e83d4e0045de9098
mlkbk <malikykone@gmail.com>
2021-08-16 17:57:08 +0000

    créer l'url pour adhesion

--- accueil/urls.py
+++ accueil/urls.py
@@ -6,6 +6,7 @@ app_name = 'accueil'
 
 urlpatterns = [
     path('', views.Accueil.as_view(), name='accueil'),
+    path('adhesion', views.Adhesion.as_view(), name='adhesion'),    
 ]
 
 handler404 = 'accueil.views.error_404_view'
