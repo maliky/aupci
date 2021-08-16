68b15db60393fd3780b5e1cc299f4950adaf2f58
mlkbk <malikykone@gmail.com>
2021-08-16 21:49:10 +0000

    ajout d'un model que nous reutiliserons dans le formulaire

--- accueil/models.py
+++ accueil/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Adherent(models.Model):
+    date_demande = models.DateTimeField(auto_now_add=True)
+    date_acceptance = models.DateTimeField(auto_now_add=False)
+
+    nom = models.CharField(max_length=50, blank=True, null=True)
+    prenom = models.CharField(max_length=50, blank=True, null=True)
+
+    numero_telephone = models.CharField(max_length=20, blank=True, null=True)
+    courriel = models.EmailField()
+    occupation = models.CharField(max_length=50, blank=True, null=True)
